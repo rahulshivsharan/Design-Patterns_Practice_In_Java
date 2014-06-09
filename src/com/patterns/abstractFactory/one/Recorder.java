ca73e72fdf092bcf43d0add1b18f45b077acfeda
Rahul Shivsharan <rahulshivsharan@gmail.com>
2014-06-09 17:19:32 +0530

    Create Recorder.java

---
+++ src/com/patterns/abstractFactory/one/Recorder.java
@@ -0,0 +1,11 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.patterns.abstractfactory.one;
+
+
+public interface Recorder {
+    void load(Media m);
+    void record(String record);
+}
