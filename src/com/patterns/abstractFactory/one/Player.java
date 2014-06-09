89171f2d23cb73e0d9b9b0297151a48387183833
Rahul Shivsharan <rahulshivsharan@gmail.com>
2014-06-09 16:38:12 +0530

    Create Player.java

---
+++ src/com/patterns/abstractFactory/one/Player.java
@@ -0,0 +1,11 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.patterns.abstractfactory.one;
+
+
+public interface Player {
+    void load(Media m);
+    void play();
+}
