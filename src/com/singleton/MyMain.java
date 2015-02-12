79d7fb2cd8d204a230e6cc906c8a09629ec07ff5
Rahul Shivsharan <postbox.rahul@hotmail.com>
2015-02-12 18:35:05 +0530

    Main class for Singlton
    Just runs the Singleton behaviour implemented in other classes

---
+++ src/com/singleton/MyMain.java
@@ -0,0 +1,46 @@
+package com.singleton;
+
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+public class MyMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Properties props = null;
+		DataSource ds = null;
+		Connection con = null;
+		ComboPooledDataSource pooledDS = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		try{
+			pooledDS = LoadDataSource.INSTANCE.getPooledDatasource();
+			con = pooledDS.getConnection();
+			ps = con.prepareStatement("select st_name from studentTbl");
+			rs = ps.executeQuery();
+			while(rs.next()){
+				System.out.println("Name : "+rs.getString("ST_NAME"));
+			}
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			try{
+				if(con != null && !con.isClosed()){
+					con.close();
+				}
+			}catch(SQLException sqle){
+				sqle.printStackTrace();
+			}
+		}
+	}
+
+}
